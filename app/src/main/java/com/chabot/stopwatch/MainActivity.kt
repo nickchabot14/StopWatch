feea7d6652901791e54e996cacd4b9f815999160
nickchabot <nicholaschabot14@stu.spusd.net>
2021-09-09 10:09:52 -0700

    Initial commit

---
+++ app/src/main/java/com/chabot/stopwatch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chabot.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCaeate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //æääāåã
+    }
+}
