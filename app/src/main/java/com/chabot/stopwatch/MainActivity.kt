0ab712336754dae76c4e2549bee6471af7310832
nickchabot <nicholaschabot14@stu.spusd.net>
2021-09-13 10:09:50 -0700

    h

--- app/src/main/java/com/chabot/stopwatch/MainActivity.kt
+++ app/src/main/java/com/chabot/stopwatch/MainActivity.kt
@@ -2,11 +2,23 @@ package com.chabot.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Chronometer
 
+lateinit var stopWatch : Chronometer
+lateinit var start : Button
+lateinit var reset : Button
 class MainActivity : AppCompatActivity() {
-    override fun onCaeate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //æääāåã
+        wirewidgets()
+        //æääāåãáà
+    }
+
+    private fun wirewidgets(){
+        stopWatch = findViewById(R.id.chronometer_main_stopwatch)
+        start = findViewById(R.id.button)
+        reset = findViewById(R.id.button2)
     }
 }
